634c976af8586f94580a4dae2be85493a7b29d3f
RWTema (other machine) <rwtema@gmail.com>
2018-07-07 10:36:04 +0100

    Rewrite teleport code to be more sensible and not kill players

--- src/main/java/com/rwtema/monkmod/advancements/criteria/MonkRequirementDescentAscent.java
+++ src/main/java/com/rwtema/monkmod/advancements/criteria/MonkRequirementDescentAscent.java
@@ -1,11 +1,14 @@
 package com.rwtema.monkmod.advancements.criteria;
 
 import com.rwtema.monkmod.data.MonkData;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.SPacketChangeGameState;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ITeleporter;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 public class MonkRequirementDescentAscent extends MonkRequirementDeath {
@@ -19,11 +22,7 @@ public class MonkRequirementDescentAscent extends MonkRequirementDeath {
 		int progress = monkData.getProgress();
 		if (monkData.increase(1, 15)) {
 			super.onDeathAvoid(player, monkData);
-
-			MinecraftServer minecraftServer = player.world.getMinecraftServer();
-			assert minecraftServer != null;
-			player.connection.sendPacket(new SPacketChangeGameState(4, 0));
-			player.connection.player = minecraftServer.getPlayerList().recreatePlayerEntity(player, player.world.provider.getDimension(), false);
+			player.changeDimension(0, (world, entity, yaw) -> entity.moveToBlockPosAndAngles(world.getTopSolidOrLiquidBlock(world.getSpawnPoint()), yaw, entity.rotationPitch));
 		} else {
 			player.sendMessage(new TextComponentTranslation("monk.final." + progress));
 		}
